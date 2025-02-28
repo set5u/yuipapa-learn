928a68364501cb846498504e7da6f90c9eced910
ajiken4610 <ajiken4610@gmail.com>
2025-02-28 20:16:29 +0900

    save

--- components/Select.vue
+++ components/Select.vue
@@ -26,7 +26,7 @@
 
 <script lang="ts">
   export const modes = ref<string[]>(
-    JSON.parse(localStorage.getItem("modes") || "[]")
+    JSON.parse(localStorage.getItem("modes") || "['A2B']")
   );
   watch(modes, () => {
     if (!modes.value.length) {
